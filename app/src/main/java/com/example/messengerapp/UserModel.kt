a7b2c37358748e80b3866dbeae394a18007661e4
Msyversson <syversson125@gmail.com>
2023-04-24 13:21:15 +0200

    first commit

---
+++ app/src/main/java/com/example/messengerapp/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.messengerapp
+
+data class UserModel(val id: Int, val name: String, val score: Int, val isHuman: Boolean) {
+
+
+    override fun toString(): String {
+        return "UserModel(id=$id, name='$name', score=$score, isHuman=$isHuman)"
+    }
+}
